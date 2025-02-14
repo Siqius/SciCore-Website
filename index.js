c847cfa352168726b8e5cb02fe51cc1305977149
Adam Strömberg <adam.stromberg1@learnet.se>
2025-02-14 01:14:01 +0100

    possible vh fix for iphone nav bar

---
+++ index.js
@@ -0,0 +1,5 @@
+let bodyHeight = window.innerHeight;
+let bodyWidth = window.innerWidth;
+
+document.querySelector("body").style.height = bodyHeight;
+document.querySelector("body").style.width = bodyWidth;
